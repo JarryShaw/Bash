447872096bf9dc123c7ef1d71cea3e1bdf2d6180
Jarry Shaw <JarryShaw@users.noreply.github.com>
2018-03-05 19:07:29 +0800

    Updated `pypi`

--- Others/pypi.sh
+++ Others/pypi.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 
-python setup.py register -r pypitest
-python setup.py sdist upload -r pypitest
+blush=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
 
-python setup.py register -r pypi
-python setup.py sdist upload -r pypi
+if [ -e setup.py ] ; then
+	echo "-*- ${green}Register in PyPi-Test${reset} -*-"
+    python setup.py register -r pypitest
+    tput clear
+    sleep 10
+    echo "-*- ${green}Upload to PyPi-Test${reset} -*-"
+    python setup.py sdist upload -r pypitest
+    tput clear
+    sleep 10
+    echo "-*- ${green}Register in PyPi${reset} -*-"
+    python setup.py register -r pypi
+    tput clear
+    sleep 10
+    echo "-*- ${green}Upload to PyPi${reset} -*-"
+    python setup.py sdist upload -r pypi
+else
+    echo "${blush}Error: do not find setup.py in $PWD${reset}"
+fi
