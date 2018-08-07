ce55828e6ab608592593a5e4e9c20a4b5d36695e
Jarry Shaw <JarryShaw@users.noreply.github.com>
2018-08-07 16:14:20 +0800

    Fixed minor bugs

--- Others/pypi.sh
+++ Others/pypi.sh
@@ -7,17 +7,17 @@ reset="\033[0m"
 if [ -e setup.py ] ; then
     mkdir eggs sdist wheels 2> /dev/null
     echo -e "-*- ${green}Upload to PyPi-Test${reset} -*-\n"
-    rm -rf build
-    mv -f dist/*.egg eggs/
-    mv -f dist/*.whl wheels/
-    mv -f dist/*.tar.gz sdist/
+    rm -rf build 2> /dev/null
+    mv -f dist/*.egg eggs/ 2> /dev/null
+    mv -f dist/*.whl wheels/ 2> /dev/null
+    mv -f dist/*.tar.gz sdist/ 2> /dev/null
     ( set -x; python3 setup.py sdist bdist_wheel; twine upload dist/* -r pypitest --skip-existing )
     tput clear
     echo -e "-*- ${green}Upload to PyPi${reset} -*-\n"
-    rm -rf build
-    mv -f dist/*.egg eggs/
-    mv -f dist/*.whl wheels/
-    mv -f dist/*.tar.gz sdist/
+    rm -rf build 2> /dev/null
+    mv -f dist/*.egg eggs/ 2> /dev/null
+    mv -f dist/*.whl wheels/ 2> /dev/null
+    mv -f dist/*.tar.gz sdist/ 2> /dev/null
     ( set -x; python3 setup.py sdist bdist_wheel; twine upload dist/* -r pypi --skip-existing )
 else
     echo -e "${blush}Error${reset}: do not find setup.py in $PWD"
--- Others/pypitest.sh
+++ Others/pypitest.sh
@@ -5,13 +5,13 @@ green="\033[96m"
 reset="\033[0m"
 
 if [ -e setup.py ] ; then
-    mkdir egg sdist wheels 2> /dev/null
+    mkdir eggs sdist wheels 2> /dev/null
     echo -e "-*- ${green}Upload to PyPi-Test${reset} -*-\n"
-    rm -rf build
-    mv -f dist/*.egg eggs/
-    mv -f dist/*.whl wheels/
-    mv -f dist/*.tar.gz sdist/
-    ( set -x; python setup.py sdist bdist_wheel; twine upload dist/* upload -r pypitest --skip-existing )
+    rm -rf build 2> /dev/null
+    mv dist/*.egg eggs/ 2> /dev/null
+    mv dist/*.whl wheels/ 2> /dev/null
+    mv dist/*.tar.gz sdist/ 2> /dev/null
+    ( set -x; python setup.py sdist bdist_wheel; twine upload dist/* -r pypitest --skip-existing )
 else
     echo -e "${blush}Error${reset}: do not find setup.py in $PWD"
 fi
