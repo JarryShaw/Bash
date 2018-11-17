45083d5ad6655ac610dd07f2f10119a1dbdb8eab
Jarry Shaw <JarryShaw@users.noreply.github.com>
2018-11-17 21:55:53 +0800

    Regular update

--- Others/bootstrap.sh
+++ Others/bootstrap.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 
+# print a trace of simple commands
+set -x
+
 # install requirements
 sudo apt-get update
 sudo apt-get install -y \
+    build-essential \
+    curl \
+    file \
+    git \
+    net-tools \
     python3 \
     python3-pip
 sudo --set-home python3 -m pip install --upgrade \
@@ -11,6 +19,24 @@ sudo --set-home python3 -m pip install --upgrade \
     setuptools \
     ipython \
     pipenv
+
+# # create user "linuxbrew"
+# adduser linuxbrew
+# usermod -aG sudo linuxbrew
+# su - linuxbrew
+# returncode=$?
+# if [[ $returncode -ne 0 ]] ; then
+#     exit $returncode
+# fi
+# logout
+
+# # install Linuxbrew
+# sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+# test -d ~/.linuxbrew && PATH="$HOME/.linuxbrew/bin:$HOME/.linuxbrew/sbin:$PATH"
+# test -d /home/linuxbrew/.linuxbrew && PATH="/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin:$PATH"
+# test -r ~/.bash_profile && echo "export PATH='$(brew --prefix)/bin:$(brew --prefix)/sbin'":'"$PATH"' >>~/.bash_profile
+# echo "export PATH='$(brew --prefix)/bin:$(brew --prefix)/sbin'":'"$PATH"' >>~/.profile
+# logout linuxbrew
 
 # prepare Pipenv
 cd ~/Desktop
